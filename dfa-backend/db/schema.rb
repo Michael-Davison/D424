b33db9072f218cb84b9f0e825fa137c510beb927
Michael-Davison <35985443+Michael-Davison@users.noreply.github.com>
2025-09-17 12:50:18 -0400

    Setup postges DB

--- dfa-backend/db/schema.rb
+++ dfa-backend/db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_17_162514) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "transactions", force: :cascade do |t|
     t.date "date"
     t.string "description"
