6a2d65ba5a198357918a00224b52e7656b91bcff
Michael-Davison <35985443+Michael-Davison@users.noreply.github.com>
2025-09-18 15:56:21 -0400

    reverse expense model negative value before save

--- dfa-backend/app/models/expense.rb
+++ dfa-backend/app/models/expense.rb
@@ -1,15 +1,9 @@
 class Expense < Transaction
-
-  before_save :make_amount_negative
-
+  
   validates :category, inclusion: { in: Transaction::EXPENSE_CATEGORIES }
 
   scope :by_category, ->(cat) { where(category: cat) }
-
-  def make_amount_negative
-    self.amount = -amount.abs
-  end
-
+  
   def self.monthly_total(user_id, month = Date.current.month, year = Date.current.year)
     where(user_id: user_id, date: Date.new(year, month, 1)..Date.new(year, month, -1)).sum(:amount)
   end
